1f97997ac2160a884f8c9f00118d4aeb87487acc
Tony Huang <tonyhuang@hotmail.co.uk>
2018-07-23 15:09:11 +0100

    Initialization. Started Cucumber + RESTassured project.

---
+++ RESTassured/src/test/java/com/qa/RESTassured/OMDBTest.java
@@ -0,0 +1,14 @@
+package com.qa.RESTassured;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OMDBTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
